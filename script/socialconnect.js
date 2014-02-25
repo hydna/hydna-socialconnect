f3c3faa4997831a48ce4db2d554a23bc2a5dff37
skaggivara <isak.wistrom@gmail.com>
2014-02-25 17:50:48 +0100

    added mode to public conn

--- script/socialconnect.js
+++ script/socialconnect.js
@@ -40,6 +40,7 @@ SocialConnect.prototype.connect = function(uuid, token){
         }
 
         self._me_channel.onclose = function(event){
+            console.log(event);
             if(event.wasDenied){
                 self.onerror && self.onerror(event.reason);
             }
@@ -48,6 +49,7 @@ SocialConnect.prototype.connect = function(uuid, token){
         self._me_channel.onsignal = function(event){
             var msg = null;
             
+            console.log(event);
             try{
                 var msg = JSON.parse(event.data);
             }catch(e){}
@@ -81,7 +83,7 @@ SocialConnect.prototype.connect = function(uuid, token){
 SocialConnect.prototype.connectPublic = function(){
     var self = this;
     if(!self._public_channel){
-        self._public_channel = new HydnaChannel(self._domain_addr+"/"+self._socialnetwork+"?"+self._uuid);
+        self._public_channel = new HydnaChannel(self._domain_addr+"/"+self._socialnetwork+"?"+self._uuid, 'rwe');
         self._public_channel.onopen = function(event){
             self._public_connected = true;
         }
@@ -94,6 +96,8 @@ SocialConnect.prototype.connectPublic = function(){
 
         self._public_channel.onsignal = function(event){
             var msg = null;
+
+            console.log("received event from public");
 
             try{
                 var msg = JSON.parse(event.data);
